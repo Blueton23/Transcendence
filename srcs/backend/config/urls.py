f0ea332bb8f9a24fea54126933f59d8fa509e4f7
David <david@example.com>
2026-07-26 14:39:40 +0200

    fix bug ruff

--- srcs/backend/config/urls.py
+++ srcs/backend/config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import path
 
+
 def api_test(request):
     return JsonResponse({
         "message": "L'API Django fonctionne",
