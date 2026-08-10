f7614365018481ab8773f10c293951d4a7f6ec53
Charlotte <charlotte.pasquier@bluewin.ch>
2026-08-10 18:37:13 +0200

    feat(step): ajout de getSteps hardcode

---
+++ srcs/frontend/src/features/step/api/stepApi.ts
@@ -0,0 +1,43 @@
+import type { Step } from "../types";
+
+export function getSteps(): Step[] {
+	return [
+		{	id: 1,
+			travelId: 1,
+			position: 1,
+			nights: 0,
+			localisation: "Montreux",
+			latitude: 46.4312,
+			longitude: 6.9107,
+			deletedAt:  null,
+			createdAt: "2026-08-10T15:27:00Z",
+			updatedAt: "2026-08-10T15:27:00Z",
+
+		},
+		{
+			id: 2,
+			travelId: 1,
+			position: 2,
+			nights: 2,
+			localisation: "Interlaken",
+			latitude: 46.6833,
+			longitude: 7.8500,
+			deletedAt:  null,
+			createdAt: "2026-08-10T18:16:00Z",
+			updatedAt: "2026-08-10T18:16:00Z",
+
+		},
+		{
+			id: 3,
+			travelId: 1,
+			position: 3,
+			nights: 2,
+			localisation: "Zermatt",
+			latitude: 46.0207,
+			longitude: 7.7491,
+			deletedAt:  null,
+			createdAt: "2026-08-10T18:19:00Z",
+			updatedAt: "2026-08-10T18:19:00Z",
+		}
+	]
+}
