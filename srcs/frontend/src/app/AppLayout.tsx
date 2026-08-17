c176b49e69107d679b1a48630626cc9dd8d17bde
Charlotte <charlotte.pasquier@bluewin.ch>
2026-08-17 17:20:22 +0200

    style: formatage prettier du code de routing

--- srcs/frontend/src/app/AppLayout.tsx
+++ srcs/frontend/src/app/AppLayout.tsx
@@ -1,13 +1,13 @@
 import { Navigation } from "./Navigation";
-import { Outlet } from "react-router"
+import { Outlet } from "react-router";
 
 export function AppLayout() {
-	return (
-		<div className="flex h-screen">
-			<Navigation />
-			<main className="flex-1 gap-2">
-				<Outlet />
-			</main>
-		</div>
-	);
+  return (
+    <div className="flex h-screen">
+      <Navigation />
+      <main className="flex-1 gap-2">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
--- srcs/frontend/src/app/Navigation.tsx
+++ srcs/frontend/src/app/Navigation.tsx
@@ -1,9 +1,11 @@
 import Text from "../shared/ui/Text";
 
 export function Navigation() {
-	return (
-		<aside>
-			<Text className = "w-56 shrink-0 border-r border-gray-200 p-4">Navigation a venir</Text>
-		</aside>
-	);
+  return (
+    <aside>
+      <Text className="w-56 shrink-0 border-r border-gray-200 p-4">
+        Navigation a venir
+      </Text>
+    </aside>
+  );
 }
