a20d12dcce32bfe4c58a70458801a1d61f858514
Charlotte <charlotte.pasquier@bluewin.ch>
2026-08-17 16:08:29 +0200

    feat(routes): ajout BrowserRouter et layout (AppLayout + Navigation)

---
+++ srcs/frontend/src/app/Navigation.tsx
@@ -0,0 +1,9 @@
+import Text from "../shared/ui/Text";
+
+export function Navigation() {
+	return (
+		<aside>
+			<Text className = "w-56 shrink-0 border-r border-gray-200 p-4">Navigation a venir</Text>
+		</aside>
+	);
+}
